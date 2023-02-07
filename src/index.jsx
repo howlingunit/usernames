d5e7187da780a9285d444038de223b3a07f1ebd9
howlingunit <tyler_pitt@outlook.com>
2023-02-07 14:23:06 +0000

    setting up frontend

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
+function App() {
+  return (
+    <h1>Usernames!!</h1>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
