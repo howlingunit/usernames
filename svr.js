97ffd1f316b6b6178da1640acda8c1f978147fa8
howlingunit <93931085+howlingunit@users.noreply.github.com>
2023-02-11 00:32:02 +0000

    bug fix

--- svr.js
+++ svr.js
@@ -4,7 +4,7 @@ import * as api from './lib/api.js'
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('access-Control-Allow_Origin', '*');
+  res.header('access-Control-Allow-Origin', '*');
   next();
 })
 
